a001a69cea1550041b6d57cfd7b65e73cc95346d
Rushiraj Gadhavi <168898251+AmAkAtch@users.noreply.github.com>
2025-08-20 13:47:59 +0530

    init commit for Banking methods day5

---
+++ day05_methods/bankingmethods/BankingMethods.java
@@ -0,0 +1,20 @@
+package day05_methods.bankingmethods;
+
+public class BankingMethods {
+
+    public static void main(String[] args) {
+
+        // user should be allowed to interact through a menu driven system
+        // deposit money
+        // winthdraw funds
+        // check balance
+        // calculate interest
+        // view summary of their account
+
+        // minimum balance requirement
+        // overdraft fees
+        String acHolderName = "John Smith";
+        double initialBalance = 1500.00;
+
+    }
+}
