85ec76ab2ed3da163c219020ff406ac8a3f60730
Rushiraj Gadhavi <168898251+AmAkAtch@users.noreply.github.com>
2025-08-20 16:39:35 +0530

    Completed Project for day5

--- day05_methods/bankingmethods/BankingMethods.java
+++ day05_methods/bankingmethods/BankingMethods.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class BankingMethods {
 
     private static final double INTEREST_RATE = 0.025;
-    private static final double OVERDRAFT_FREE = 35.00;
     private static final double MINIMUM_BALANCE = 100.00;
 
     // main method
@@ -19,9 +18,12 @@ public class BankingMethods {
         greetuser(acHolderName);
 
         do {
+            scanner.nextLine();
+            System.out.println("Press any Enter to continue...");
+            scanner.nextLine();
+
             displayMenu();
             choice = scanner.nextInt();
-
             switch (choice) {
                 case 1:
                     initialBalance = depositeMoney(scanner, initialBalance);
@@ -30,13 +32,13 @@ public class BankingMethods {
                     initialBalance = withdrawMoney(scanner, initialBalance);
                     break;
                 case 3:
-                    checkBalance();
+                    checkBalance(initialBalance);
                     break;
                 case 4:
-                    calculateInterest();
+                    calculateInterest(initialBalance);
                     break;
                 case 5:
-                    displayUserInfo();
+                    displayUserInfo(acHolderName, initialBalance);
                     break;
                 case 6:
                     break;
@@ -45,7 +47,7 @@ public class BankingMethods {
                     break;
             }
         } while (choice != 6);
-
+        System.out.println("Thanks for Using out Services, Come again!");
     }
 
     // function to Greet user
@@ -53,6 +55,19 @@ public class BankingMethods {
         System.out.println("=================================");
         System.out.println("Welcome to the Banking System " + userName);
         System.out.println("=================================");
+    }
+
+    // Function to display menu to user
+    public static void displayMenu() {
+        /* This function will Display Banking menu to make choice later */
+
+        System.out.println("=== Banking Menu ===");
+        System.out.println("1. Deposite Money");
+        System.out.println("2. Withdraw Money");
+        System.out.println("3. Check Balance");
+        System.out.println("4. Calculate Interest");
+        System.out.println("5. Account Summary");
+        System.out.println("6. Exit");
     }
 
     // function to let user deposite money
@@ -90,6 +105,10 @@ public class BankingMethods {
             System.out.println("You have successfully withdrawn " + withdrawAmount);
             initialBalance -= withdrawAmount;
             System.out.println("Your new account balance is $" + initialBalance);
+            if (initialBalance < MINIMUM_BALANCE) {
+                System.out.println("Your Balance is lower than the limit $" + MINIMUM_BALANCE
+                        + ", Please Deposite money immediately to avoid charges.");
+            }
             return initialBalance;
         } else if (withdrawAmount > initialBalance) {
             System.out.println("Sorry you do not have enough Balance to make withdrawl, try again...");
@@ -100,32 +119,41 @@ public class BankingMethods {
         }
     }
 
-    // Function to display menu to user
-    public static void displayMenu() {
-        /* This function will Display Banking menu to make choice later */
-
-        System.out.println("=== Banking Menu ===");
-        System.out.println("1. Deposite Money");
-        System.out.println("2. Withdraw Money");
-        System.out.println("3. Check Balance");
-        System.out.println("4. Calculate Interest");
-        System.out.println("5. Account Summary");
-        System.out.println("6. Exit");
-    }
-
     // function to let user check Balance
-    public static void checkBalance() {
+    public static void checkBalance(double initialBalance) {
+        // This function accepts initial balance and just displays the current balance
+        // for user
+        System.out.println("Your Current account Balance is $" + initialBalance);
 
     }
 
     // function to let user calculate interest
-    public static void calculateInterest() {
-
+    public static void calculateInterest(double initialBalance) {
+        System.out.println("You currently have $" + initialBalance + " in your account.");
+        System.out.println("Based on that Balance and Current Interest rate of " + INTEREST_RATE);
+        double yearlyInterest = initialBalance * INTEREST_RATE;
+        double monthlyInterest = yearlyInterest / 12;
+        System.out.println("Your Monthly interest : $" + monthlyInterest);
+        System.out.println("Your Yearly interest : $" + yearlyInterest);
     }
 
     // function to let user display User info
-    public static void displayUserInfo() {
+    public static void displayUserInfo(String username, double initialBalance) {
+        System.out.println("Username : " + username);
+        System.out.println("Current Account Balance : " + initialBalance);
+        printAccountStatus(initialBalance);
+        calculateInterest(initialBalance);
+    }
 
+    // function to print Account Status
+    public static void printAccountStatus(double initialBalance) {
+        if (initialBalance > MINIMUM_BALANCE * 10) {
+            System.out.println("Status : Premium Account");
+        } else if (initialBalance > MINIMUM_BALANCE) {
+            System.out.println("Status : Standard Account");
+        } else {
+            System.out.println("Low Balance Warning, Add Balance to Avoide charges");
+        }
     }
 
 }
