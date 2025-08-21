aa8030367254814d0a6ddd0c6d5e1a55198671ba
Rushiraj Gadhavi <168898251+AmAkAtch@users.noreply.github.com>
2025-08-21 11:58:06 +0530

    basic operations like find max, index and average on array

--- day06_array_basics/basics/ArrayBasics.java
+++ day06_array_basics/basics/ArrayBasics.java
@@ -12,9 +12,33 @@ public class ArrayBasics {
         return maxNum;
     }
 
+    // function to find average
+    public static double findAverage(int[] numbers) {
+        double totalSum = 0;
+        for (int num : numbers) {
+            totalSum += num;
+        }
+
+        return (totalSum / numbers.length);
+    }
+
+    // function to find the index of specific value
+    public static int findValue(int[] numbers, int value) {
+        if (numbers.length == 0)
+            return 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (value == numbers[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         int[] numbers = { 1, 5, 6, 2, 20, 21, 44, 2, 33, 45, 232324, 4535343, 35, 45, 425, 32423, 4, 234, 234, 234, 234,
                 234, 234, 23, 434, 5635, 645, 67456, 6745, 534, 52, 5, 566234, 23, 456 };
-        System.out.println(findMax(numbers));
+        System.out.println("Max number :" + findMax(numbers));
+        System.out.println("Average :" + findAverage(numbers));
+        System.out.println("Index of Number 33 in array :" + findValue(numbers, 33));
     }
 }
