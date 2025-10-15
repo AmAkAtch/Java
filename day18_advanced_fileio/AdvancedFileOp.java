7ea7c56945c2ffed0751bfa8ac3fff29af4c1f2c
Rushiraj Gadhavi <168898251+AmAkAtch@users.noreply.github.com>
2025-10-15 12:31:38 +0530

    Basic file and Directory operations completed from Day 18

--- day18_advanced_fileio/AdvancedFileOp.java
+++ day18_advanced_fileio/AdvancedFileOp.java
@@ -6,13 +6,43 @@ public class AdvancedFileOp {
     public static void main(String[] args) {
 
         // just load the file if it exists
-        File file = new File("Employees.txt");
+        File file = new File("day18_advanced_fileio/data/Employees.txt");
 
-        // Check if the file is loaded or not
+        // Check if the file is loaded or not - ALWAYS CHECK IF FILE EXISTS OR NOT
+        // BEFORE TRYING ANY OPERATIONS
         if (file.exists()) {
             System.out.println("File exists");
         } else {
             System.out.println("File does not exists");
         }
+
+        // File operations
+        System.out.println("File name: " + file.getName());
+        System.out.println("File Path :" + file.getPath());
+        System.out.println("File absolute Path: " + file.getAbsolutePath());
+        System.out.println("File size : " + file.length() + "bytes");
+        System.out.println("Is file? " + file.isFile() + "\nis Directory? " + file.isDirectory());
+
+        File directory = new File("day18_advanced_fileio/data");
+
+        if (!directory.exists()) {
+            directory.mkdir();
+            System.out.println("Directory created successfully");
+        }
+
+        File[] files = directory.listFiles();
+
+        if (files != null && files.length > 0) {
+            System.out.println("List of files in Directory");
+
+            for (File f : files) {
+                System.out.println(f.getName() + (f.isDirectory() ? "(Folder)" : ""));
+            }
+        } else {
+            System.out.println(directory.getName() + " is Empty Directory !!");
+        }
+
+        // READING NON BINARY FILES LIKE IMAGES, AUDIO, VIDEO, COMPILED CLASS,
+        // SERIALIZED JAVA OBJECTS
     }
 }
